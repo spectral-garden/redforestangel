21cc9c7eb829d6183aea22b50c578fbfe7c4e11f
spectral-garden <124816402+spectral-garden@users.noreply.github.com>
2023-02-13 23:39:13 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+// video js
+function deferVideo() {
+
+  //defer html5 video loading
+$("video source").each(function() {
+  var sourceFile = $(this).attr("data-src");
+  $(this).attr("src", sourceFile);
+  var video = this.parentElement;
+  video.load();
+ 
+//video.play();
+});
+
+}
+window.onload = deferVideo;
+
+
+//
+
+
+
+
+
+
+
+// Set up the scene
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+
+// Set up the camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+camera.position.y = 2;
+
+// Set up the renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Set up the colors to cycle through
+var colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff];
+
+// Set up the tree of life geometry
+var treeGeometry = new THREE.TorusKnotBufferGeometry(11, 0.1, 100, 9);
+var treeMaterial = new THREE.MeshBasicMaterial({ color: colors[0] });
+var tree = new THREE.Mesh(treeGeometry, treeMaterial);
+scene.add(tree);
+
+// Set up the text geometry
+var fontLoader = new THREE.FontLoader();
+fontLoader.load('https://cdn.jsdelivr.net/npm/three@0.121.1/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+  var textGeometry = new THREE.TextGeometry("RED FOREST ANGEL", {
+    font: font,
+    size: 0.5,
+    height: 0.1,
+    curveSegments: 12,
+    bevelEnabled: false,
+    bevelThickness: 10,
+    bevelSize: 8,
+    bevelSegments: 5
+  });
+  var textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  var text = new THREE.Mesh(textGeometry, textMaterial);
+  
+ // Set up the color change interval
+ var colorIndex = 0;
+ setInterval(function() {
+   textMaterial.color.setHex(colors[colorIndex % colors.length]);
+   colorIndex++;
+ }, 1000);
+ 
+
+
+  text.position.y = -2;
+  scene.add(text);
+});
+
+// Set up the color change interval
+var currentColorIndex = 0;
+setInterval(function () {
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+  treeMaterial.color.setHex(colors[currentColorIndex]);
+}, 5000);
+
+// Render loop
+function render() {
+  requestAnimationFrame(render);
+
+  tree.rotation.x += 0.01;
+  tree.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+// Add an animation to follow the mouse
+document.addEventListener('mousemove', function (event) {
+  tree.position.x = -(event.clientX / window.innerWidth) * 10 + 5;
+  tree.position.y = (event.clientY / window.innerHeight) * 5 - 2.5;
+});
+
+// Start the render loop
+render();
+//
+
+
---
+++ app2.js
@@ -0,0 +1,125 @@
+// video js
+function deferVideo() {
+
+  //defer html5 video loading
+$("video source").each(function() {
+  var sourceFile = $(this).attr("data-src");
+  $(this).attr("src", sourceFile);
+  var video = this.parentElement;
+  video.load();
+ 
+//video.play();
+});
+
+}
+window.onload = deferVideo;
+
+
+
+
+
+
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+// Set up the colors to cycle through
+var colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff];
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
+
+
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+const rainGeometry = new THREE.Geometry();
+for (let i = 0; i < 1000; i++) {
+  const rainDrop = new THREE.Vector3(
+    THREE.MathUtils.randFloatSpread(10),
+    THREE.MathUtils.randFloatSpread(10),
+    THREE.MathUtils.randFloatSpread(10)
+  );
+  rainGeometry.vertices.push(rainDrop);
+}
+
+const rainMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.1, sizeAttenuation: false });
+
+const rain = new THREE.Points(rainGeometry, rainMaterial);
+scene.add(rain);
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(rain);
+  if (intersects.length > 0) {
+    const index = intersects[0].index;
+    const rainDrop = rainGeometry.vertices[index];
+    rainDrop.y += 0.1;
+  } else {
+    rainGeometry.vertices.forEach((rainDrop) => {
+      if (rainDrop.y < -5) {
+        rainDrop.y = THREE.MathUtils.randFloat(10, 20);
+        rainDrop.x = THREE.MathUtils.randFloatSpread(10);
+        rainDrop.z = THREE.MathUtils.randFloatSpread(10);
+      } else {
+        rainDrop.y -= 0.1;
+      }
+    });
+  }
+  rainGeometry.verticesNeedUpdate = true;
+
+// Set up the colors to cycle through
+var colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff];
+
+// Set up the text geometry
+var fontLoader = new THREE.FontLoader();
+fontLoader.load('https://cdn.jsdelivr.net/npm/three@0.121.1/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+  var textGeometry = new THREE.TextGeometry("RED FOREST ANGEL", {
+    font: font,
+    size: 0.5,
+    height: 0.1,
+    curveSegments: 12,
+    bevelEnabled: false,
+    bevelThickness: 10,
+    bevelSize: 8,
+    bevelSegments: 5
+  });
+  var textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  var text = new THREE.Mesh(textGeometry, textMaterial);
+  
+// Set up the color change interval
+var colorIndex = 0;
+setInterval(function() {
+  textMaterial.color.setHex(colors[colorIndex % colors.length]);
+  textMaterial.needsUpdate = true; // <-- Add this line
+  colorIndex++;
+}, 1000);
+
+
+  text.position.y = -2;
+  scene.add(text);
+});
+
+
+
+  renderer.render( scene, camera );
+}
+
+function onDocumentMouseMove(event) {
+  event.preventDefault();
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+animate();
